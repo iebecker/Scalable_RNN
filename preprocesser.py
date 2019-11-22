f7058f2125b0c399bc1f0e45684b266ee5d82daa
ibecker <iebecker@uc.cl>
2019-11-22 16:05:24 -0500

    Removed Dev variables, serialize is a general function now

--- preprocesser.py
+++ preprocesser.py
@@ -15,7 +15,7 @@ class Preprocesser():
     transform and save the data'''
     def __init__(self, file_train, file_dev, save_dir='./',
                  max_L=40000, min_L=500, min_N=0, max_N = 2000, w=4, s=2, w_time=True,
-                 num_cores=8, train0_size = 0.95, train1_size=0.5, dev_size=0.25, 
+                 num_cores=8, train0_size = 0.95, train1_size=0.5, dev_size=0.25,
                  lc_parameters = None, inference= False, inference_folder = None):
 
         # Set inference mode
@@ -29,10 +29,10 @@ class Preprocesser():
         # We already imposed a minimum number of poitns per light curve
         # Impose a minimum of points per light curve
         if self.inference:
-            self.max_L = 1e6
+            self.max_L = 1e6 #Arbitrary
             self.min_L = 0
             self.min_N = 0
-            self.max_N = 1e6
+            self.max_N = 1e6 #Arbitrary
         else:
             self.max_L = max_L
             self.min_L = min_L
@@ -41,7 +41,7 @@ class Preprocesser():
 
         # Dataset info
         self.file_train = file_train
-        self.file_dev = file_dev
+        # self.file_dev = file_dev
         self.read_datasets()
 
         # Extract classes and the number of them
@@ -67,12 +67,13 @@ class Preprocesser():
         self.lc_parameters = lc_parameters
 
         # Set the train/test/val sizes
-        self.train0_size = train0_size
-        self.traindev_size = 1 - train0_size
+        self.train_size = train_size
+        # self.traindev_size = 1 - train0_size
 
-        self.train1_size = train1_size
-        self.dev_size = dev_size
-        self.test_size = 1 - dev_size - train1_size
+        self.train_size = train_size
+        # self.dev_size = dev_size
+        # self.test_size = 1 - dev_size - train1_size
+        self.test_size = 1 - train_size
 
         # Auxiliary functions to parallelize
         # Parallel does not work with class functions (for some reason)
@@ -81,7 +82,11 @@ class Preprocesser():
         self.__func_serialize = utils.serialize
 
     def filter_train(self):
-                # Objects that fulfill the number of datapoints condition
+        '''Filter the objects to be read.
+        First by imposing restriction to the number of data points.
+        Second, by extracting a random sample of up uo max_L elements
+            per category.'''
+        # Objects that fulfill the number of datapoints condition
         bol21 = self.data_train.N >=self.min_N
         bol22 = self.data_train.N <=self.max_N
         bol2 = np.logical_and(bol21,bol22)
@@ -96,11 +101,9 @@ class Preprocesser():
 
             # Limit the minimum number of light curves
             if sel.shape[0] < self.min_L:
-
                 # Update the classes
                 self.classes.remove(i)
                 self.num_classes = len(self.classes)
-
                 # Skip the class
                 continue
 
@@ -110,36 +113,24 @@ class Preprocesser():
                 num = min(self.max_L, sel.shape[0])
                 # Get a random sample
                 sel = sel.sample(num, replace=False, axis=0)
-
             dfs.append(sel)
-
         # Join the dataframes of each class together
         self.data_train = pd.concat(dfs)
-
-    def filter_dev(self):
-        f0 = self.data_dev.N >=self.min_N
-        f1 = self.data_dev.N <=self.max_N
-        filter_ = f0 & f1
-        self.data_dev = self.data_dev[filter_]
 
 
     def read_datasets(self):
         '''Read the dataset, extract the LCs information, with the class and ID.
         Filter the specified number of LC per class, so it does not read everything '''
         # Read stars Data
-        self.data_train = pd.read_csv(self.file_train, usecols=['ID', 'Address','Class','N'])
-        self.data_dev = pd.read_csv(self.file_dev, usecols=['ID', 'Address','Class','N'])
+        self.data = pd.read_csv(self.file_train, usecols=['ID', 'Address','Class','N'])
 
         # Extract classes and the number of them
-        self.classes = list(set(self.data_dev.Class))
-        self.num_classes = len(self.data_dev)
-        
+        self.classes = list(set(self.data.Class))
+        self.num_classes = len(self.data)
+
         # Filter train according to number of observations and elements per class
         self.filter_train()
-        self.filter_dev()
 
-        # Join all data in one df
-        self.data = pd.concat([self.data_train, self.data_dev])
 
     def paralell_read(self):
         '''Read the data using n_jobs. Store them in a dict where the classes
@@ -174,7 +165,6 @@ class Preprocesser():
 
         # Transform the data into matrices
         self.Labels = np.array(self.Labels)
-        # self.Ns = np.array(self.Ns)
         self.Matrices = np.array(self.Matrices)
         self.IDs = np.array(self.IDs)
 
@@ -189,15 +179,15 @@ class Preprocesser():
             self.Matrices = self.Matrices[ind]
             self.IDs = self.IDs[ind]
 
-    def indices(self, train_ids, traindev_ids, dev_ids, test_ids):
+    def indices(self, train_ids, val_ids, test_ids):
         ind = range(len(self.IDs))
         ind_dict = dict(zip(self.IDs, ind))
+
         ind_train = list(map(ind_dict.get, train_ids))
-        ind_traindev = list(map(ind_dict.get, traindev_ids))
-        ind_dev = list(map(ind_dict.get, dev_ids))
+        ind_val = list(map(ind_dict.get, val_ids))
         ind_test = list(map(ind_dict.get, test_ids))
 
-        return ind_train, ind_traindev, ind_dev, ind_test
+        return ind_train, ind_test, ind_val
 
     def cls_metadata(self, labels):
         keys, values = np.unique(labels, return_counts= True)
@@ -209,44 +199,35 @@ class Preprocesser():
         hist['Total'] = len(labels)
         return hist
 
-    def split_train_dev(self):
+    def split_train(self):
+        self.data_train= self.data_train.set_index('ID')
 
-        self.data_train = self.data_train.set_index('ID')
-        self.data_dev = self.data_dev.set_index('ID')
-
-        train_ids0, traindev_ids = train_test_split(
-            self.data_train.index.values, 
-            train_size=self.train0_size, 
+        train_ids, test_val_ids = train_test_split(
+            self.data_train.index.values,
+            train_size=self.train_size,
             stratify=self.data_train.Class)
-        train_ids1, test_ids = train_test_split(
-            self.data_dev.index.values, 
-            train_size=self.train1_size, 
-            stratify=self.data_dev.Class)
-        dev_ids, test_ids = train_test_split(
-            test_ids, 
-            train_size=self.dev_size/(1-self.train1_size),
-            stratify=self.data_dev.loc[test_ids].Class)
-        train_ids = np.concatenate([train_ids0, train_ids1])
 
-        ind_train, ind_traindev, ind_dev, ind_test = self.indices(train_ids, traindev_ids, dev_ids, test_ids)
+        test_ids, val_ids = train_test_split(
+            test_val_ids,
+            train_size=self.test_size/(1-self.train_size),
+            stratify=self.data.loc[test_val_ids].Class)
+
+        ind_train, ind_test, ind_val = self.indices(train_ids, val_ids, test_ids)
 
         self.Matrices_train = self.Matrices[ind_train]
         self.Labels_train = self.Labels[ind_train]
         self.IDs_train = self.IDs[ind_train]
 
-        self.Matrices_traindev = self.Matrices[ind_traindev]
-        self.Labels_traindev = self.Labels[ind_traindev]
-        self.IDs_traindev = self.IDs[ind_traindev]
-
-        self.Matrices_dev = self.Matrices[ind_dev]
-        self.Labels_dev = self.Labels[ind_dev]
-        self.IDs_dev = self.IDs[ind_dev]
+        self.Matrices_val = self.Matrices[ind_val]
+        self.Labels_val = self.Labels[ind_val]
+        self.IDs_val = self.IDs[ind_val]
 
         self.Matrices_test = self.Matrices[ind_test]
         self.Labels_test = self.Labels[ind_test]
         self.IDs_test = self.IDs[ind_test]
 
-        splits_labels = [self.Labels_train, self.Labels_traindev, self.Labels_dev, self.Labels_test]
+        splits_labels = [self.Labels_train, self.Labels_val, self.Labels_test]
+
         values = [self.cls_metadata(labels) for labels in splits_labels]
         keys = ['Train set', 'Train-dev set', 'Dev set', 'Test set']
         metadata = dict(zip(keys, values))
@@ -256,39 +237,56 @@ class Preprocesser():
     def serialize_all(self):
         '''Serialize the data into TFRecords.'''
 
-        with open(self.save_dir+'Train.tfrecord', 'w') as f:
-            writer = tf.io.TFRecordWriter(f.name)
-            for i in range(len(self.IDs_train)):
-                ex = self.__func_serialize(self.Matrices_train[i], self.Labels_train[i],self.IDs_train[i])
-                writer.write(ex.SerializeToString())
+        self.serialize(self.Matrices_train[i],
+                       self.Labels_train[i],
+                       self.IDs_train[i]],
+                       self.save_dir+'Train.tfrecord')
 
-        with open(self.save_dir+'Traindev.tfrecord', 'w') as f:
-            writer = tf.io.TFRecordWriter(f.name)
-            for i in range(len(self.IDs_traindev)):
-                ex = self.__func_serialize(self.Matrices_traindev[i], self.Labels_traindev[i],self.IDs_traindev[i])
-                writer.write(ex.SerializeToString())
+        self.serialize(self.Matrices_val[i],
+                       self.Labels_val[i],
+                       self.IDs_val[i]],
+                       self.save_dir+'Val.tfrecord')
 
-        with open(self.save_dir+'Dev.tfrecord', 'w') as f:
-            writer = tf.io.TFRecordWriter(f.name)
-            for i in range(len(self.IDs_dev)):
-                ex = self.__func_serialize(self.Matrices_dev[i], self.Labels_dev[i], self.IDs_dev[i])
-                writer.write(ex.SerializeToString())
+        self.serialize(self.Matrices_test[i],
+                       self.Labels_test[i],
+                       self.IDs_test[i]],
+                       self.save_dir+'Test.tfrecord')
 
-        with open(self.save_dir+'Test.tfrecord', 'w') as f:
-            writer = tf.io.TFRecordWriter(f.name)
-            for i in range(len(self.IDs_test)):
-                ex = self.__func_serialize(self.Matrices_test[i], self.Labels_test[i], self.IDs_test[i])
-                writer.write(ex.SerializeToString())
+        # with open(self.save_dir+'Train.tfrecord', 'w') as f:
+        #     writer = tf.io.TFRecordWriter(f.name)
+        #     for i in range(len(self.IDs_train)):
+        #         ex = self.__func_serialize(self.Matrices_train[i], self.Labels_train[i],self.IDs_train[i])
+        #         writer.write(ex.SerializeToString())
+
+        # with open(self.save_dir+'Val.tfrecord', 'w') as f:
+        #     writer = tf.io.TFRecordWriter(f.name)
+        #     for i in range(len(self.IDs_val)):
+        #         ex = self.__func_serialize(self.Matrices_val[i], self.Labels_val[i], self.IDs_val[i])
+        #         writer.write(ex.SerializeToString())
+        #
+        # with open(self.save_dir+'Test.tfrecord', 'w') as f:
+        #     writer = tf.io.TFRecordWriter(f.name)
+        #     for i in range(len(self.IDs_test)):
+        #         ex = self.__func_serialize(self.Matrices_test[i], self.Labels_test[i], self.IDs_test[i])
+        #         writer.write(ex.SerializeToString())
 
 
     def serialize_inference(self, save_path=None):
+        '''Serialize data for inference.'''
         if save_path is None:
             save_path = self.inference_folder+'Inference.tfrecord'
-        '''Serialize data for inference.'''
+
+        self.serialize(self.Matrices[i],
+                        self.Labels[i],
+                        self.IDs[i]
+                        save_path)
+
+    def serialize(self, Matrices, Labels, IDs, save_path):
+        '''Serialize objects given the data and path.'''
         with open(save_path, 'w') as f:
             writer = tf.io.TFRecordWriter(f.name)
-            for i in range(len(self.IDs)):
-                ex = self.__func_serialize(self.Matrices[i], self.Labels[i],self.IDs[i])
+            for i in range(len(IDs)):
+                ex = self.__func_serialize(Matrices[i], Labels[i], IDs[i])
                 writer.write(ex.SerializeToString())
 
     def write_metadata_process(self):
@@ -307,7 +305,6 @@ class Preprocesser():
         path = self.save_dir+'metadata_preprocess.json'
         with open(path, 'w') as fp:
             json.dump(metadata, fp)
-
         # Save the light curve parameters for the pandas call
         np.savez(self.save_dir+'lc_parameters',lc_parameters = self.lc_parameters)
 
