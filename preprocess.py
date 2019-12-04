ba1ea8d76344f8e7242c8af6ffd22a834e5d5b7e
ibecker <iebecker@uc.cl>
2019-12-04 11:34:27 -0500

    Changed the preprocess to match the current behavior.

--- preprocess.py
+++ preprocess.py
@@ -1,23 +1,27 @@
 import os
 import preprocesser as p
 
-data_dir  = '/Users/Javiera/Desktop/Harvard/Code/Data/features/ts-model/datasets/'
-data_save = data_dir + 'train/all/'
-file_train = data_dir + 'preprocess/Gaia_xmatch.dat'
-file_dev = data_dir + 'preprocess/Gaia_xmatch.dat'
 
-min_L = 500
-max_L = 5000
+data_dir  = './Databases/Gaia/data_2/G/'
+data_save = './Serialized/Saved_Data/'
+file_train = './Splits/Train.dat'
+file_test = './Splits/Test.dat'
+file_val = './Splits/Val.dat'
+
+
+min_L = 100
+max_L = 50
 min_N = 15
 max_N = 2000
 w = 4
 s = 2
-lc_parameters = {'header':0, 'na_filter':False,'sep':',','usecols':['time', 'mag', 'mag_err']}
-num_cores = 4
+time = False
 
-preprocesser = p.Preprocesser(file_train=file_train, file_dev=file_dev, save_dir='../datasets/train/all/',
-               max_L=max_L, min_L=min_L, min_N=min_N, max_N=max_N, w=w, s=s,
-               num_cores=num_cores, lc_parameters=lc_parameters)
+lc_parameters = {'header':0, 'na_filter':False,'sep':',','usecols':['time', 'mag', 'mag_err']}
+num_cores = 7
+
+preprocesser = Prep.Preprocesser(max_L=max_L, min_L=min_L, min_N=min_N, max_N=max_N, w=w, s=s,
+               num_cores=num_cores, lc_parameters=lc_parameters,w_time = time)
 
 # Excecute the inference
-preprocesser.prepare()
+preprocesser.prepare(file_train=file_train, save_dir=data_save, train_size=file_train, test_size=file_test, val_size = file_val)
